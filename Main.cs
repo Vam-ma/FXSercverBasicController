432c0095f95b65d2f34c961f265efda1882acb96
Vamma <58887108+Vam-ma@users.noreply.github.com>
2024-08-18 21:07:49 +0300

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,23 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using static CitizenFX.Core.Native.API;
+using System;
+
+namespace BasicControllerClient
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            API.RegisterCommand("Test", new Action(Test), false);
+        }
+        private void Test()
+        {
+            Ped player = Game.Player.Character;
+            Model[] models = GetAllVehicleModels();
+            Random random = new Random();
+            int i = random.Next(models.Length);
+            World.CreateVehicle(models[i], player.Position);
+        }
+    }
+}
