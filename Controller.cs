a6d4ace65ca6abdc10ffd7cef439a81d9c71cc9e
Vamma <58887108+Vam-ma@users.noreply.github.com>
2024-08-18 20:39:01 +0300

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,19 @@
+﻿using CitizenFX.Core;
+
+using static CitizenFX.Core.Native.API;
+
+namespace BasicControllerClient
+{
+    public class Controller : BaseScript
+    {
+        [EventHandler("onClientResourceStart")]
+        private void OnClientResourceStart(string resourceName)
+        {
+            if (resourceName != GetCurrentResourceName())
+                return;
+
+            Debug.WriteLine("Succes");
+
+        }
+    }
+}
